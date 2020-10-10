7a5a2633ebab752cfc18804fbc8650ea17d39bac
Alex Withington-Smith <alex@awithsmith.com>
2020-10-10 09:45:58 +0100

    added test for #make_guess

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -1,9 +1,17 @@
 require 'hangman'
 
 describe Word do
-  describe "#display_word" do
+  describe '#display_word' do
     it "returns '_' in place of each char by default" do
-      expect(Word.new('apple').display_word).to eq("_ _ _ _ _")
+      no_letters_guessed = Word.new('apple')
+      expect(no_letters_guessed.display_word).to eq("_ _ _ _ _")
+    end
+  end
+  describe '#make_guess' do
+    it "adds to array of correctly guessed characters" do
+      test_guesses = Word.new('example')
+      test_guesses.make_guess('e')
+      expect(test_guesses.correct_letters).to eq(['e'])
     end
   end
 end
