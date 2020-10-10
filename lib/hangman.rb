82bfe34c9d4e059fbdadf3138da80fce38eb4ce8
Alex Withington-Smith <alex@awithsmith.com>
2020-10-10 09:50:26 +0100

    passing first #make_guess test

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,9 +1,18 @@
 class Word
+  attr_reader :correct_letters
+
   def initialize(word)
     @word = word
+    @correct_letters = []
   end
 
   def display_word
     '_ ' * (@word.length - 1) + '_'
+  end
+
+  def make_guess(letter)
+    if @word.split('').include?(letter)
+      correct_letters.push(letter)
+    end
   end
 end
