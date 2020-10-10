bea83552bc48969686396eb19f7832f2f0c1a6f6
Alex Withington-Smith <alex@awithsmith.com>
2020-10-10 09:54:06 +0100

    added some more outlier test cases for #make_guess

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -7,11 +7,25 @@ describe Word do
       expect(no_letters_guessed.display_word).to eq("_ _ _ _ _")
     end
   end
+
   describe '#make_guess' do
     it "adds to array of correctly guessed characters" do
       test_guesses = Word.new('example')
       test_guesses.make_guess('e')
       expect(test_guesses.correct_letters).to eq(['e'])
+    end
+
+    it "works with multiple guesses" do
+      test_guesses = Word.new('example')
+      test_guesses.make_guess('e')
+      test_guesses.make_guess('x')
+      expect(test_guesses.correct_letters).to eq(['e', 'x'])
+    end
+
+    it "doesn't return incorrect letters" do
+      test_guesses = Word.new('example')
+      test_guesses.make_guess('z')
+      expect(test_guesses.correct_letters).to eq([])
     end
   end
 end
